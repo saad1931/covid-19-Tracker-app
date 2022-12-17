f295ca8f4299d708b60784aecf6f0a65e4e0beb0
Muhammad Saad <muhammadsaad3119@gmail.com>
2022-12-17 15:55:01 +0500

    splash screen

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:covid_tracker/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+      ),
+      home: const SplashScren(),
+    );
+  }
+}
---
+++ lib/screens/world_stats.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WorldStats extends StatefulWidget {
+  const WorldStats({super.key});
+
+  @override
+  State<WorldStats> createState() => _WorldStatsState();
+}
+
+class _WorldStatsState extends State<WorldStats> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
